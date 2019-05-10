9cfe812b8f42cb30df2561acaf1854189259d0f8
elmurotyangiboev <elmurotyangiboev@elmurots-imac.fios-router.home>
2019-05-10 15:50:05 -0400

    added generateAlert() method to project

--- DailyPracticeSelenium/src/main/java/selenium/JSExecutorConcepts.java
+++ DailyPracticeSelenium/src/main/java/selenium/JSExecutorConcepts.java
@@ -30,16 +30,18 @@ public class JSExecutorConcepts {
 		driver.get("https://www.pnc.com");
 
 		WebElement pncLogo = driver.findElement(By.xpath("//img[@alt='PNC']"));
-		
+
 		flashObject(pncLogo, driver); // highlight WebElement
 		drawBorder(pncLogo, driver); // draw a border around the WebElement
-		takeScreenshot("pncLogo", ".jpg");
-		driver.quit();
+		takeScreenshot("pncLogo", ".jpg"); // take a screenshot
+		generateAlert(driver, "There is an issue with pncLogo on Home page");
+
+//		driver.quit();
 
 	}
-//	/*
-//	 ************** flash Objects method is used to flash Webelement **********************
-	 
+
+//	 ************** flashObject() method is used to flash WebElement **********************
+
 	public static void flashObject(WebElement element, WebDriver driver) {
 		js = ((JavascriptExecutor) driver);
 		String bgcolor = element.getCssValue("backgroundColor");
@@ -58,9 +60,8 @@ public class JSExecutorConcepts {
 		} catch (InterruptedException e) {
 		}
 	}
-	
-//	 **************************************************************************************
-//	 */
+
+//	 ************** drawBorder() method is used to draw border for WebElement ****************
 
 	public static void drawBorder(WebElement element, WebDriver driver) {
 		js = (JavascriptExecutor) driver;
@@ -70,12 +71,23 @@ public class JSExecutorConcepts {
 		} catch (InterruptedException e) {
 		}
 	}
-	
-	public static void takeScreenshot(String fileName,String extension) throws IOException {
-		
+
+//	 **************** takeScreenshot() method is used to take a Screenshot *******************
+
+	public static void takeScreenshot(String fileName, String extension) throws IOException {
+
 		String timestamp = new SimpleDateFormat("yyyy_MM_dd__hh_mm_ss").format(new Date());
-		File src = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
-		FileHandler.copy(src, new File("/Users/elmurotyangiboev/git/CucumberBDDFramework/CucumberBDDFramework/src/Screenshots/" + fileName+" "+timestamp+extension));
-		
+		File src = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+		FileHandler.copy(src,
+				new File("/Users/elmurotyangiboev/git/CucumberBDDFramework/CucumberBDDFramework/src/Screenshots/"
+						+ fileName + " " + timestamp + extension));
 	}
+
+//	 **************** generateAlert() method is used to generate alert for WebElement *******************
+
+	public static void generateAlert(WebDriver driver, String message) {
+		js = (JavascriptExecutor) driver;
+		js.executeScript("alert('" + message + "')");
+	}
+
 }
